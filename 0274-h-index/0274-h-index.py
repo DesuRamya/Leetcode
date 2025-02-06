58c232d8d524c6aaec956a0468b70d9202370265
Desu.Ramya <119840662+DesuRamya@users.noreply.github.com>
2025-02-06 18:09:25 +0530

    Time: 0 ms (100%), Space: 18 MB (54.53%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,14 +1,9 @@
 class Solution:
     def hIndex(self, nums: List[int]) -> int:
+        nums.sort()
         n=len(nums)
-        o=0
-        nums1=sorted(nums,reverse=True)
         for i in range(n):
-            print("op")
-            if nums1[i]>=i+1:
-                o=i+1
-        if nums==[0]:
-            return 0
-        if n==1:
-            return 1
-        return o
+            if nums[i]>=n-i:
+                return n-i
+        return 0
+        
