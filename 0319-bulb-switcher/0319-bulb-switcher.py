a15f0cde6e44428fdc144ceab4e0101cd864e6e9
Desu.Ramya <119840662+DesuRamya@users.noreply.github.com>
2025-02-28 21:09:53 +0530

    Time: 0 ms (100%), Space: 17.8 MB (29.69%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.py
+++ 0319-bulb-switcher/0319-bulb-switcher.py
@@ -1,10 +1,3 @@
 class Solution:
     def bulbSwitch(self, n: int) -> int:
-        c=0
-        for i in range(1,n+1):
-            if i*i<=n:
-                print(i)
-                c+=1
-            else:
-                break
-        return c
+        return int(math.sqrt(n))
