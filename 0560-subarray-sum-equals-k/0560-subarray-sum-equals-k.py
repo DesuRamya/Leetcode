c966c58c247bc32b810af775452ecdb861cf1f60
Desu.Ramya <119840662+DesuRamya@users.noreply.github.com>
2025-03-04 22:40:57 +0530

    Time: 43 ms (21.42%), Space: 20.5 MB (20.11%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -5,8 +5,10 @@ class Solution:
         for i in range(1,len(nums)):
             prefixSum.append(prefixSum[i-1]+nums[i])
         d,res={},0
-        d[0]=1
+        # d[0]=1
         for i in range(len(nums)):
+            if prefixSum[i] == k:
+                res+=1
             if prefixSum[i]-k in d:
                 res+=d[prefixSum[i]-k]
             if prefixSum[i] in d:
