1d5a19f32b751ebcd35dcfc08cda409d55254732
Desu.Ramya <119840662+DesuRamya@users.noreply.github.com>
2026-01-03 19:23:49 +0530

    Time: 0 ms (100%), Space: 18.8 MB (22.57%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
@@ -3,9 +3,6 @@ class Solution:
         d ={}
         for i in nums:
             if i in d:
-                d[i]+=1
+                return i
             else:
                 d[i]=1
-        for i in d:
-            if d[i]>1:
-                return i
